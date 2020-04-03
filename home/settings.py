b4600c9e14c2b8322e465210d5fce5fd05ffc9fa
ekkopy <thiagognlcn@gmail.com>
2020-04-03 14:08:58 -0300

    fix :star:

--- home/settings.py
+++ home/settings.py
@@ -125,13 +125,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
    'static'
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = os.path.join(BASE_DIR, 'static')
 
 # Media files
 MEDIA_ROOT = 'media'
